362017a9e28e9e616b864b1ef318984efc58aab4
JadeMatrix <jadematrix.art@gmail.com>
2019-08-24 16:13:23 -0400

    Split command-line argument configuration into its own submodule

---
+++ anime_manager/arguments.py
@@ -0,0 +1,70 @@
+import argparse
+import pathlib
+
+
+class AbsPathAction( argparse.Action ):
+    """`argparse` action for converting strings to `pathlib.Path`s"""
+    def __call__( self, parser, namespace, values, option_string = None ):
+        try:
+            values = pathlib.Path( values ).resolve()
+        except TypeError:
+            pass
+        setattr( namespace, self.dest, values )
+
+parser = argparse.ArgumentParser(
+    description = "JadeMatrix's anime torrent manager"
+)
+
+parser.add_argument(
+    "-d",
+    "--database",
+    metavar  = "FILENAME",
+    action   = AbsPathAction,
+    help     = "YAML torrent database file to watch for changes",
+    required = True
+)
+parser.add_argument(
+    "-t",
+    "--transmission",
+    metavar  = "ADDRESS[:PORT]",
+    help     = "Transmission RPC server to use",
+    required = True
+)
+parser.add_argument(
+    "-c",
+    "--cache-dir",
+    metavar  = "DIRNAME",
+    action   = AbsPathAction,
+    help     = "persistent cache directory",
+    required = True
+)
+parser.add_argument(
+    "-g",
+    "--log-file",
+    metavar  = "FILENAME",
+    action   = AbsPathAction,
+    help     = "log file to use (defaults to 'log' in the cache directory",
+    required = False
+)
+parser.add_argument(
+    "-l",
+    "--log-level",
+    metavar  = "LEVEL",
+    choices  = [
+        "CRITICAL",
+        "ERROR",
+        "WARNING",
+        "INFO",
+        "DEBUG",
+        "NOTSET",
+    ],
+    default  = "INFO",
+    help     = "standard Python logging level to use",
+    required = False
+)
+parser.add_argument(
+    "--no-trash",
+    action   = "store_true",
+    help     = "when removing files/directories, delete rather than trash them",
+    required = False
+)
--- anime_manager/daemon.py
+++ anime_manager/daemon.py
@@ -1,7 +1,7 @@
+import anime_manager.arguments
 import anime_manager.database
 import anime_manager.torrents
 
-import argparse
 import logging
 import os
 import pathlib
@@ -14,74 +14,6 @@ import yaml
 
 
 log = logging.getLogger( __name__ )
-
-
-class AbsPathAction( argparse.Action ):
-    """`argparse` action for converting strings to `pathlib.Path`s"""
-    def __call__( self, parser, namespace, values, option_string = None ):
-        try:
-            values = pathlib.Path( values ).resolve()
-        except TypeError:
-            pass
-        setattr( namespace, self.dest, values )
-
-parser = argparse.ArgumentParser(
-    description = "JadeMatrix's anime torrent manager"
-)
-
-parser.add_argument(
-    "-d",
-    "--database",
-    metavar  = "FILENAME",
-    action   = AbsPathAction,
-    help     = "YAML torrent database file to watch for changes",
-    required = True
-)
-parser.add_argument(
-    "-t",
-    "--transmission",
-    metavar  = "ADDRESS[:PORT]",
-    help     = "Transmission RPC server to use",
-    required = True
-)
-parser.add_argument(
-    "-c",
-    "--cache-dir",
-    metavar  = "DIRNAME",
-    action   = AbsPathAction,
-    help     = "persistent cache directory",
-    required = True
-)
-parser.add_argument(
-    "-g",
-    "--log-file",
-    metavar  = "FILENAME",
-    action   = AbsPathAction,
-    help     = "log file to use (defaults to 'log' in the cache directory",
-    required = False
-)
-parser.add_argument(
-    "-l",
-    "--log-level",
-    metavar  = "LEVEL",
-    choices  = [
-        "CRITICAL",
-        "ERROR",
-        "WARNING",
-        "INFO",
-        "DEBUG",
-        "NOTSET",
-    ],
-    default  = "INFO",
-    help     = "standard Python logging level to use",
-    required = False
-)
-parser.add_argument(
-    "--no-trash",
-    action   = "store_true",
-    help     = "when removing files/directories, delete rather than trash them",
-    required = False
-)
 
 
 class AutoManageTorrentsHandler( watchdog.events.FileSystemEventHandler ):
@@ -138,7 +70,7 @@ class AutoManageTorrentsHandler( watchdog.events.FileSystemEventHandler ):
 
 
 def run( argv = sys.argv[ 1 : ] ):
-    args = parser.parse_args( argv )
+    args = anime_manager.arguments.parser.parse_args( argv )
     
     args.cache_dir.mkdir( parents = True, exist_ok = True )
     
