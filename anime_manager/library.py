2a6570224bcea28d14ac64f828b1effe84024e55
JadeMatrix <jadematrix.art@gmail.com>
2019-10-14 10:47:11 -0400

    Refactor cleanup: split up updates in `library.update()` into utility functions

--- anime_manager/library.py
+++ anime_manager/library.py
@@ -149,6 +149,283 @@ def relative_link_pair( source, dest ):
     return { "source" : source, "dest" : dest }
 
 
+def update_old(
+    server,
+    cache,
+    database,
+    hashes,
+    new_cache,
+    stati,
+    trash,
+    dry_run
+):
+    """Clean up old links & torrents
+    
+    Utility function for `update()`
+    
+    Args:
+        server (torrents.TransmissionServer):
+                            The Transmission server to use as a reference
+        cache (dict):       Cached flat database
+        database (dict):    Newly loaded full database
+        hashes (set[str]):  Torrent hashes to remove from library, pre-generated
+                            for reuse
+        new_cache (dict):   New flatdb to insert new state into
+        stati (dict):       A lookup of show titles to their status, pre-
+                            generated for reuse
+        trash (pathlib.Path|None):
+                            Trash directory (see `filesystem.trash_item()`)
+        dry_run (bool):     Whether to skip actually executing actions
+    """
+    
+    def iter_remove_links():
+        for hash in hashes:
+            for dest in cache[ hash ][ "files" ].keys():
+                yield dest
+    
+    anime_manager.filesystem.remove_links( iter_remove_links(), trash, dry_run )
+    
+    server.remove_torrents( hashes, trash, dry_run )
+
+
+def update_keep(
+    server,
+    cache,
+    database,
+    hashes,
+    new_cache,
+    stati,
+    trash,
+    dry_run
+):
+    """Check & modify remaining torrents & links
+    
+    Utility function for `update()`
+    
+    Args:
+        server (torrents.TransmissionServer):
+                            The Transmission server to use as a reference
+        cache (dict):       Cached flat database
+        database (dict):    Newly loaded full database
+        hashes (set[str]):  Torrent hashes to check for updates, pre-generated
+                            for reuse
+        new_cache (dict):   New flatdb to insert new state into
+        stati (dict):       A lookup of show titles to their status, pre-
+                            generated for reuse
+        trash (pathlib.Path|None):
+                            Trash directory (see `filesystem.trash_item()`)
+        dry_run (bool):     Whether to skip actually executing actions
+    """
+    
+    for hash in hashes:
+        new_cache[ hash ] = {
+            "source"   : cache[ hash ][ "source"    ],
+            "location" : cache[ hash ][ "location"  ],
+            "archived" : cache[ hash ][ "archived"  ],
+            "files"    : {}
+        }
+    
+    # Move torrents
+    torrents_source  = []
+    torrents_move    = []
+    torrents_archive = []
+    for hash in hashes:
+        new_source = database[ "torrents" ][ hash ][ "source" ]
+        if cache[ hash ][ "source" ] != new_source :
+            torrents_source.append( {
+                "hash"   : hash,
+                "source" : new_source,
+            } )
+        
+        download_to = (
+            database[ "directories" ][ "torrents" ]
+            / year_quarter_for_torrent( database, hash )
+        )
+        
+        if download_to != cache[ hash ][ "location" ]:
+            torrents_move.append( {
+                "hash"     : hash,
+                "location" : download_to,
+            } )
+        
+        if cache[ hash ][ "archived" ] != database[ "torrents" ][ hash ][ "archived" ]:
+            torrents_archive.append( {
+                "hash"    : hash,
+                "started" : not database[ "torrents" ][ hash ][ "archived" ],
+            } )
+    
+    server.source_torrents( torrents_source, trash, dry_run )
+    for torrent in torrents_source:
+        new_cache[ torrent[ "hash" ] ][ "source" ] = torrent[ "source" ]
+    
+    server.move_torrents( torrents_move, trash, dry_run )
+    for torrent in torrents_move:
+        new_cache[ torrent[ "hash" ] ][ "location" ] = torrent[ "location" ]
+    
+    server.status_torrents( torrents_archive, trash, dry_run )
+    for torrent in torrents_archive:
+        new_cache[ torrent[ "hash" ] ][ "archived" ] = not torrent[ "started" ]
+    
+    torrent_names = {}
+    get_names_tries = 0
+    while True:
+        try:
+            torrent_names = server.torrent_names( hashes )
+            break
+        except anime_manager.torrents.RPCError:
+            if dry_run:
+                # Placeholders for dry run
+                torrent_names = dict(
+                    ( h, "$HASH:{}$".format( h ) )
+                    for h in hashes
+                )
+                break
+            elif get_names_tries < 5:
+                log.warning(
+                    "not all torrent names available yet, retrying..."
+                )
+                get_names_tries -= 1
+                time.sleep( 1 )
+            else:
+                raise
+    
+    for hash in hashes:
+        links_remove = []
+        links_add    = {}
+        
+        files = {}
+        for episode in database[ "torrents" ][ hash ][ "episodes" ]:
+            dest = database[ "directories" ][
+                stati[ episode[ "show" ][ "title" ] ]
+            ] / show_link_for_episode( database, episode )
+            source = (
+                new_cache[ hash ][ "location" ]
+                / torrent_names[ hash ]
+            )
+            if "file" in episode:
+                source = source / episode[ "file" ]
+            # Replace placeholder suffix with source's
+            files[ dest.with_suffix( source.suffix ) ] = source
+        
+        new_dests = set( files )
+        old_dests = set( cache[ hash ][ "files" ] )
+        
+        # Links that straight up don't exist anymore
+        links_remove += old_dests - new_dests
+        # Links that have changed what they point to
+        for dest in old_dests & new_dests:
+            if files[ dest ] != cache[ hash ][ "files" ][ dest ]:
+                links_remove.append( dest )
+                links_add[ dest ] = files[ dest ]
+        anime_manager.filesystem.remove_links( links_remove, trash, dry_run )
+        
+        # Links to add
+        for dest in new_dests - old_dests:
+            links_add[ dest ] = files[ dest ]
+        
+        links_add2 = {}
+        for dest, source in links_add.items():
+            l = relative_link_pair( source, dest )
+            links_add2[ l[ "dest" ] ] = l[ "source" ]
+        anime_manager.filesystem.add_links( links_add2, trash, dry_run )
+        
+        new_cache[ hash ][ "files" ] = files
+
+
+def update_new(
+    server,
+    cache,
+    database,
+    hashes,
+    new_cache,
+    stati,
+    trash,
+    dry_run
+):
+    """Add new torrents & links
+    
+    Utility function for `update()`
+    
+    Args:
+        server (torrents.TransmissionServer):
+                            The Transmission server to use as a reference
+        cache (dict):       Cached flat database
+        database (dict):    Newly loaded full database
+        hashes (set[str]):  Torrent hashes to add to the library, pre-generated
+                            for reuse
+        new_cache (dict):   New flatdb to insert new state into
+        stati (dict):       A lookup of show titles to their status, pre-
+                            generated for reuse
+        trash (pathlib.Path|None):
+                            Trash directory (see `filesystem.trash_item()`)
+        dry_run (bool):     Whether to skip actually executing actions
+    """
+    
+    # First add torrents...
+    add_torrents = []
+    for hash in hashes:
+        download_to = (
+            database[ "directories" ][ "torrents" ]
+            / year_quarter_for_torrent( database, hash )
+        )
+        add_torrents.append( {
+            "source"   : database[ "torrents" ][ hash ][ "source" ],
+            "location" : download_to,
+            "started"  : not database[ "torrents" ][ hash ][ "archived" ],
+        } )
+        new_cache[ hash ] = {
+            "source"   : database[ "torrents" ][ hash ][ "source" ],
+            "location" : download_to,
+            "archived" : database[ "torrents" ][ hash ][ "archived" ],
+        }
+    server.add_torrents( add_torrents, trash, dry_run )
+    
+    # ... then add links once we can get torrent names
+    torrent_names = {}
+    get_names_tries = 0
+    while True:
+        try:
+            torrent_names = server.torrent_names( hashes )
+            break
+        except anime_manager.torrents.RPCError:
+            if dry_run:
+                # Placeholders for dry run
+                torrent_names = dict(
+                    ( h, "$HASH:{}$".format( h ) )
+                    for h in hashes
+                )
+                break
+            elif get_names_tries < 5:
+                log.warning(
+                    "not all torrent names available yet, retrying..."
+                )
+                get_names_tries -= 1
+                time.sleep( 1 )
+            else:
+                raise
+    
+    for hash in hashes:
+        links = {}
+        for episode in database[ "torrents" ][ hash ][ "episodes" ]:
+            dest = database[ "directories" ][
+                stati[ episode[ "show" ][ "title" ] ]
+            ] / show_link_for_episode( database, episode )
+            source = (
+                new_cache[ hash ][ "location" ]
+                / torrent_names[ hash ]
+            )
+            if "file" in episode:
+                source = source / episode[ "file" ]
+            # Replace placeholder suffix with source's
+            links[ dest.with_suffix( source.suffix ) ] = source
+        links_add = {}
+        for dest, source in links.items():
+            l = relative_link_pair( source, dest )
+            links_add[ l[ "dest" ] ] = l[ "source" ]
+        anime_manager.filesystem.add_links( links_add, trash, dry_run )
+        new_cache[ hash ][ "files" ] = links
+
+
 def update( server, cache, database, trash, dry_run = False ):
     """Run a library update
     
@@ -179,199 +456,21 @@ def update( server, cache, database, trash, dry_run = False ):
             stati[ show[ "title" ] ] = status
     
     try:
-        # Clean up old links & torrents ########################################
-        remove_hashes = old_hashes - new_hashes
-        def iter_remove_links():
-            for hash in remove_hashes:
-                for dest in cache[ hash ][ "files" ].keys():
-                    yield dest
-        anime_manager.filesystem.remove_links( iter_remove_links(), trash, dry_run )
-        server.remove_torrents( remove_hashes, trash, dry_run )
-        
-        # Check & modify remaining torrents & links ############################
-        keep_hashes = old_hashes & new_hashes
-        
-        for hash in keep_hashes:
-            new_cache[ hash ] = {
-                "source"   : cache[ hash ][ "source"    ],
-                "location" : cache[ hash ][ "location"  ],
-                "archived" : cache[ hash ][ "archived"  ],
-                "files"    : {}
-            }
-        
-        # Move torrents
-        torrents_source  = []
-        torrents_move    = []
-        torrents_archive = []
-        for hash in keep_hashes:
-            new_source = database[ "torrents" ][ hash ][ "source" ]
-            if cache[ hash ][ "source" ] != new_source :
-                torrents_source.append( {
-                    "hash"   : hash,
-                    "source" : new_source,
-                } )
-            
-            download_to = (
-                database[ "directories" ][ "torrents" ]
-                / year_quarter_for_torrent( database, hash )
+        for action, hashes in (
+            ( update_old , old_hashes - new_hashes, ),
+            ( update_keep, old_hashes & new_hashes, ),
+            ( update_new , new_hashes - old_hashes, ),
+        ):
+            action(
+                server,
+                cache,
+                database,
+                hashes,
+                new_cache,
+                stati,
+                trash,
+                dry_run
             )
-            
-            if download_to != cache[ hash ][ "location" ]:
-                torrents_move.append( {
-                    "hash"     : hash,
-                    "location" : download_to,
-                } )
-            
-            if cache[ hash ][ "archived" ] != database[ "torrents" ][ hash ][ "archived" ]:
-                torrents_archive.append( {
-                    "hash"    : hash,
-                    "started" : not database[ "torrents" ][ hash ][ "archived" ],
-                } )
-        
-        server.source_torrents( torrents_source, trash, dry_run )
-        for torrent in torrents_source:
-            new_cache[ torrent[ "hash" ] ][ "source" ] = torrent[ "source" ]
-        
-        server.move_torrents( torrents_move, trash, dry_run )
-        for torrent in torrents_move:
-            new_cache[ torrent[ "hash" ] ][ "location" ] = torrent[ "location" ]
-        
-        server.status_torrents( torrents_archive, trash, dry_run )
-        for torrent in torrents_archive:
-            new_cache[ torrent[ "hash" ] ][ "archived" ] = not torrent[ "started" ]
-        
-        torrent_names = {}
-        get_names_tries = 0
-        while True:
-            try:
-                torrent_names = server.torrent_names( keep_hashes )
-                break
-            except anime_manager.torrents.RPCError:
-                if dry_run:
-                    # Placeholders for dry run
-                    torrent_names = dict(
-                        ( h, "$HASH:{}$".format( h ) )
-                        for h in keep_hashes
-                    )
-                    break
-                elif get_names_tries < 5:
-                    log.warning(
-                        "not all torrent names available yet, retrying..."
-                    )
-                    get_names_tries -= 1
-                    time.sleep( 1 )
-                else:
-                    raise
-        
-        for hash in keep_hashes:
-            links_remove = []
-            links_add    = {}
-            
-            files = {}
-            for episode in database[ "torrents" ][ hash ][ "episodes" ]:
-                dest = database[ "directories" ][
-                    stati[ episode[ "show" ][ "title" ] ]
-                ] / show_link_for_episode( database, episode )
-                source = (
-                    new_cache[ hash ][ "location" ]
-                    / torrent_names[ hash ]
-                )
-                if "file" in episode:
-                    source = source / episode[ "file" ]
-                # Replace placeholder suffix with source's
-                files[ dest.with_suffix( source.suffix ) ] = source
-            
-            new_dests = set( files )
-            old_dests = set( cache[ hash ][ "files" ] )
-            
-            # Links that straight up don't exist anymore
-            links_remove += old_dests - new_dests
-            # Links that have changed what they point to
-            for dest in old_dests & new_dests:
-                if files[ dest ] != cache[ hash ][ "files" ][ dest ]:
-                    links_remove.append( dest )
-                    links_add[ dest ] = files[ dest ]
-            anime_manager.filesystem.remove_links( links_remove, trash, dry_run )
-            
-            # Links to add
-            for dest in new_dests - old_dests:
-                links_add[ dest ] = files[ dest ]
-            
-            links_add2 = {}
-            for dest, source in links_add.items():
-                l = relative_link_pair( source, dest )
-                links_add2[ l[ "dest" ] ] = l[ "source" ]
-            anime_manager.filesystem.add_links( links_add2, trash, dry_run )
-            
-            new_cache[ hash ][ "files" ] = files
-        
-        # Add new torrents & links #############################################
-        add_hashes = new_hashes - old_hashes
-        
-        # First add torrents...
-        add_torrents = []
-        for hash in add_hashes:
-            download_to = (
-                database[ "directories" ][ "torrents" ]
-                / year_quarter_for_torrent( database, hash )
-            )
-            add_torrents.append( {
-                "source"   : database[ "torrents" ][ hash ][ "source" ],
-                "location" : download_to,
-                "started"  : not database[ "torrents" ][ hash ][ "archived" ],
-            } )
-            new_cache[ hash ] = {
-                "source"   : database[ "torrents" ][ hash ][ "source" ],
-                "location" : download_to,
-                "archived" : database[ "torrents" ][ hash ][ "archived" ],
-            }
-        server.add_torrents( add_torrents, trash, dry_run )
-        
-        # ... then add links once we can get torrent names
-        torrent_names = {}
-        get_names_tries = 0
-        while True:
-            try:
-                torrent_names = server.torrent_names( add_hashes )
-                break
-            except anime_manager.torrents.RPCError:
-                if dry_run:
-                    # Placeholders for dry run
-                    torrent_names = dict(
-                        ( h, "$HASH:{}$".format( h ) )
-                        for h in add_hashes
-                    )
-                    break
-                elif get_names_tries < 5:
-                    log.warning(
-                        "not all torrent names available yet, retrying..."
-                    )
-                    get_names_tries -= 1
-                    time.sleep( 1 )
-                else:
-                    raise
-        
-        for hash in add_hashes:
-            links = {}
-            for episode in database[ "torrents" ][ hash ][ "episodes" ]:
-                dest = database[ "directories" ][
-                    stati[ episode[ "show" ][ "title" ] ]
-                ] / show_link_for_episode( database, episode )
-                source = (
-                    new_cache[ hash ][ "location" ]
-                    / torrent_names[ hash ]
-                )
-                if "file" in episode:
-                    source = source / episode[ "file" ]
-                # Replace placeholder suffix with source's
-                links[ dest.with_suffix( source.suffix ) ] = source
-            links_add = {}
-            for dest, source in links.items():
-                l = relative_link_pair( source, dest )
-                links_add[ l[ "dest" ] ] = l[ "source" ]
-            anime_manager.filesystem.add_links( links_add, trash, dry_run )
-            new_cache[ hash ][ "files" ] = links
-    
     except Exception as e:
         log.error( "{}.{} thrown while updating, returning for re-raise".format(
             e.__class__.__module__,
